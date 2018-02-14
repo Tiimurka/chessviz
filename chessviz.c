c226806324bc80f42946fc3d21cc47c7c4cae553
Исанбаев Тимур Рамильевич <iv722s08@lwp12.csc.local>
2018-02-14 15:15:00 +0700

    Добавили фигурок

--- chessviz.c
+++ chessviz.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-	struct Piece {
-	bool color;
-	char type;
+
+int main(void){
+	char  board [7][7];
+	board [0][0] = 'r';
+	board [0][1] = 'n';
+	board [0][2] = 'b';
+	board [0][3] = 'q';
+	board [0][4] = 'k';
+	board [0][5] = 'b';
+	board [0][6] = 'n';
+	board [0][7] = 'r';
+	int i, j;
+	for (i=0; i<7; ++i){
+		for (j=0; j<7; ++j) {
+			printf ("%c", board [i][j]);
+		}
+		printf ("\n");
 	}
+	
+	return 0;
+}
 	
 	
