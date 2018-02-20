9ec47a33fe6a0b92f3ef722a057d817c4536580d
Tiimurka <timur22211@gmail.com>
2018-02-21 02:03:03 +0700

    More figures!

--- chessviz.c
+++ chessviz.c
@@ -2,6 +2,7 @@
 
 int main(void){
 	char  board [8][8] = {0};
+	int i, j;
 	board [0][0] = 'r';
 	board [0][1] = 'n';
 	board [0][2] = 'b';
@@ -10,9 +11,20 @@ int main(void){
 	board [0][5] = 'b';
 	board [0][6] = 'n';
 	board [0][7] = 'r';
-	int i, j;
-	for (i=0; i<7; ++i){
-		for (j=0; j<7; ++j) {
+	for (i = 0; i <= 7; ++i)
+		board [1][i] = 'p';
+	board [7][0] = 'R';
+	board [7][1] = 'N';
+	board [7][2] = 'B';
+	board [7][3] = 'Q';
+	board [7][4] = 'K';
+	board [7][5] = 'B';
+	board [7][6] = 'N';
+	board [7][7] = 'R';
+	for (i = 0; i <= 7; ++i)
+		board [6][i] = 'P';
+	for (i=0; i<=7; ++i){
+		for (j=0; j<=7; ++j) {
 			printf ("%c", board [i][j]);
 		}
 		printf ("\n");
