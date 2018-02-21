68cf84beb3ad706e8375c6f2996897f38abc3719
Исанбаев Тимур Рамильевич <iv722s08@lwp12.csc.local>
2018-02-21 14:21:09 +0700

    Codestyle changes

--- chessviz.c
+++ chessviz.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 
-int main(void){
-	char  board [8][8] = {0};
+int main(void) {
+	char board [8][8] = {0};
 	int i, j;
-	board [0][0] = 'r';
-	board [0][1] = 'n';
-	board [0][2] = 'b';
-	board [0][3] = 'q';
-	board [0][4] = 'k';
-	board [0][5] = 'b';
-	board [0][6] = 'n';
-	board [0][7] = 'r';
+	board[0][0] = 'r';
+	board[0][1] = 'n';
+	board[0][2] = 'b';
+	board[0][3] = 'q';
+	board[0][4] = 'k';
+	board[0][5] = 'b';
+	board[0][6] = 'n';
+	board[0][7] = 'r';
 	for (i = 0; i <= 7; ++i)
-		board [1][i] = 'p';
-	board [7][0] = 'R';
-	board [7][1] = 'N';
-	board [7][2] = 'B';
-	board [7][3] = 'Q';
-	board [7][4] = 'K';
-	board [7][5] = 'B';
-	board [7][6] = 'N';
-	board [7][7] = 'R';
+		board[1][i] = 'p';
+	board[7][0] = 'R';
+	board[7][1] = 'N';
+	board[7][2] = 'B';
+	board[7][3] = 'Q';
+	board[7][4] = 'K';
+	board[7][5] = 'B';
+	board[7][6] = 'N';
+	board[7][7] = 'R';
 	for (i = 0; i <= 7; ++i)
-		board [6][i] = 'P';
-	for (i=0; i<=7; ++i){
+		board[6][i] = 'P';
+	for (i=0; i<=7; ++i) {
 		for (j=0; j<=7; ++j) {
-			printf ("%c", board [i][j]);
+			printf("%c", board [i][j]);
 		}
-		printf ("\n");
+		printf("\n");
 	}
 	
 	return 0;
