afb9bbf1ab01d45f2287ffeb380c3b0badcc516f
Tiimurka <timur22211@gmail.com>
2018-04-17 19:18:26 +0700

    Splitted on different files

---
+++ board_print_plain.h
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+char board_create (char board[8][8])
+{
+	int i;
+	board[0][0] = 'r';
+	board[0][1] = 'n';
+	board[0][2] = 'b';
+	board[0][3] = 'q';
+	board[0][4] = 'k';
+	board[0][5] = 'b';
+	board[0][6] = 'n';
+	board[0][7] = 'r';
+	for (i = 0; i <= 7; ++i)
+		board[1][i] = 'p';
+	board[7][0] = 'R';
+	board[7][1] = 'N';
+	board[7][2] = 'B';
+	board[7][3] = 'Q';
+	board[7][4] = 'K';
+	board[7][5] = 'B';
+	board[7][6] = 'N';
+	board[7][7] = 'R';
+	for (i = 0; i <= 7; ++i)
+		board[6][i] = 'P';
+	return 0;
+}
+
+void board_print (char board[8][8])
+{
+	int i, j;
+	for (i=0; i<=7; ++i) {
+		for (j=0; j<=7; ++j) {
+			printf("%c", board [i][j]);
+		}
+		printf("\n");
+	}
+		return;
+}	
---
+++ main.c
@@ -0,0 +1,10 @@
+#include "board_print_plain.h"
+
+int main(void) {
+	char board [8][8] = {0};
+	board_create (board);
+	board_print (board);
+	return 0;
+}
+	
+	
